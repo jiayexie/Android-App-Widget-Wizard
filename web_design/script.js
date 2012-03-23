3d6937829d6fb74a2b813afe71a2737b279072d7
Qingya Shu <shuqingyasylvia@gmail.com>
2012-03-23 17:27:29 +0000

    1.Descriptions: "attribute" to "function", add Chinese, as well. 2.Add ".drag" class, support drag and move. 3.Change "blank_panel"'s background-image resource, still debugging....
    git-svn-id: http://i.pku.edu.cn/svn/aaww@5 0219ba12-6bc3-4faa-bf3d-573e8937644a

--- web_design/script.js
+++ web_design/script.js
@@ -1,17 +1,68 @@
 // JavaScript Document
-function WidgetProject(widgetName, author, height, width, sizeStr){
-	
-	
+function createWidgetProject(_widgetName, _author, _height, _width){
+	var tmpProject=new Object;
+	tmpProject.widgetName=_widgetName;
+	tmpProject.author=_author;
+	tmpProject.height=_height.valueOf() * 70 - 30;
+	tmpProject.width=_width.valueOf() * 70 - 30;
+	alert(height + this.width);
+	tmpProject.heightStr=this.height.toString()+"px";
+	tmpProject.widthStr=this.width.toString()+"px";
+	alert(this.heightStr + this.widthStr);
+	return tmpProject;
 }
+var w;
 
 function getStarted(){
-	//get initial parameters
-	WidgetProject.widgetName=document.name_form.widget_name.value;	
-	WidgetProject.author=document.author_form.author.value;
-	var sStr = document.size_form.size.value;
-	WidgetProject.sizeStr=sStr+".png";
-	WidgetProject.height=(sStr.substring(5,6)).valueOf();
-	WidgetProject.width=sStr.substring(7,8).valueOf();
+	//get initial parameters & create a widget
+	w = createWidgetProject(document.name_form.widget_name.value, 
+								document.author_form.author.value,
+								document.size_form.size_row.value,
+								document.size_form.size_column.value);
 	
 }
+function updateRow(){
+	w.heightStr=(document.size_form.size_row.value.valueOf()*70-30).toString()+"px";
+}
+function updateColumn(){
+	w.widthStr=(document.size_form.size_column.value.valueOf()*70-30).toString()+"px";
+}
 
+//=============================
+
+var dragobject={
+	z: 0, x: 0, y: 0, offsetx : null, offsety : null, targetobj : null, dragapproved : 0,
+	initialize:function(){
+		document.onmousedown=this.drag
+		document.onmouseup=function(){this.dragapproved=0}
+	},
+	drag:function(e){
+		var evtobj=window.event? window.event : e
+		this.targetobj=window.event? event.srcElement : e.target
+		if (this.targetobj.className=="drag"){
+			this.dragapproved=1
+			if (isNaN(parseInt(this.targetobj.style.left))){
+				this.targetobj.style.left=0
+			}
+			if (isNaN(parseInt(this.targetobj.style.top))){
+				this.targetobj.style.top=0
+			}
+			this.offsetx=parseInt(this.targetobj.style.left)
+			this.offsety=parseInt(this.targetobj.style.top)
+			this.x=evtobj.clientX
+			this.y=evtobj.clientY
+			if (evtobj.preventDefault)
+				evtobj.preventDefault()
+			document.onmousemove=dragobject.moveit
+		}
+	},
+	moveit:function(e){
+		var evtobj=window.event? window.event : e
+		if (this.dragapproved==1){
+			this.targetobj.style.left=this.offsetx+evtobj.clientX-this.x+"px"
+			this.targetobj.style.top=this.offsety+evtobj.clientY-this.y+"px"
+			return false
+		}
+	}
+}
+dragobject.initialize()
