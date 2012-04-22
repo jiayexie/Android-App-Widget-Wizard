c22544985216b2cf7713f641a340165f9763e46e
Qingya Shu <shuqingyasylvia@gmail.com>
2012-04-22 13:27:58 +0000

    导入jqueryUI库，drag我就直接用它了- -
    git-svn-id: http://i.pku.edu.cn/svn/aaww@9 0219ba12-6bc3-4faa-bf3d-573e8937644a

--- web_design/script.js
+++ web_design/script.js
@@ -1,5 +1,44 @@
 // JavaScript Document
 
+$(document).ready(function(){
+$("#start_button").click(function(){
+	$("#wrapper").hide();
+	$("#work_station").css("display", "block");
+	$("#widget_container").css("height", WidgetProject.height+"px");
+	$("#widget_container").css("width", WidgetProject.width+"px");
+});
+$("#new_textview").click(function(){
+	$("#widget_container").append(createComponent(1));
+	$("#manager").append(addToManager(1));
+	$(".drag").draggable({containment: "parent"});
+});
+$("#new_button").click(function(){
+	$("#widget_container").append(createComponent(2));
+	$("#manager").append(addToManager(2));
+	$(".drag").draggable({containment: "parent"});
+});
+$("#new_imageview").click(function(){
+	$("#widget_container").append(createComponent(3));
+	$("#manager").append(addToManager(3));
+	$(".drag").draggable({containment: "parent"});
+});
+$("#new_imagebutton").click(function(){
+	$("#widget_container").append(createComponent(4));
+	$("#manager").append(addToManager(4));
+	$(".drag").draggable({containment: "parent"});
+});
+$("#horizontalLayout").click(function(){
+	$("#widget_container").append(createComponent(5));
+	$("#manager").append(addToManager(5));
+	$(".drag").draggable({containment: "parent"});
+});
+$("#verticalLayout").click(function(){
+	$("#widget_container").append(createComponent(6));
+	$("#manager").append(addToManager(6));
+	$(".drag").draggable({containment: "parent"});
+});
+});
+
 /*
 工程对象widgetProject
 （该类唯一全区变量实例w在加载时被创建）
@@ -20,11 +59,8 @@ WidgetProject ={
 	},
 }
 
-function onLoad(){
-}
-
 //点击start之后初始化WidgetProject各种参数
-//全局变量u是插件单位为1在网页上显示的长度。对于app widget来说，实际的长度是70*u
+//全局变量u是插件单位为1在网页上显示的长度。对于app widget来说，实际的长度需要除以u乘以70
 function getStarted(){
 	WidgetProject.author=document.author_form.author.value;
 	WidgetProject.widgetName=document.name_form.widget_name.value;
@@ -41,30 +77,39 @@ globalComponentCounter = 0;//全局的部件计数器
 
 //在widget_container中添加新的部件
 var createComponent=function(cClass){
+	var str;
 	switch (cClass){
 		// create a new textview
 		case 1:
-		str = "<div id=\"component" + globalComponentCounter + "\" title=\"textview id=" + globalComponentCounter + "\" class=\"drag\">textview</div>";
+		str = "<div id=\"component" + globalComponentCounter + "\" title=\"textview id=" + globalComponentCounter + "\" class=\"drag component\">textview</div>";
 		break;
 		// create a new button
 		case 2:
-		str = "<img id=\"component" + globalComponentCounter + "\" src=\"btn.png\" title=\"button id=" + globalComponentCounter + "\"  height=\"80px\" width=\"120px\" class=\"drag\" />";
+		str = "<img id=\"component" + globalComponentCounter + "\" src=\"btn.png\" title=\"button id=" + globalComponentCounter + "\"  height=\"80px\" width=\"120px\" class=\"drag component\" />";
 		break;
 		// create a new imageview
 		case 3:
-		str = "<img id=\"component" + globalComponentCounter + "\" src=\"img.png\" title=\"image id=" + globalComponentCounter + "\"  height=\"80px\" width=\"80px\" class=\"drag\" />";
+		str = "<img id=\"component" + globalComponentCounter + "\" src=\"img.png\" title=\"image id=" + globalComponentCounter + "\"  height=\"80px\" width=\"80px\" class=\"drag component\" />";
 		break;
 		// create a new imagebutton
 		case 4:
-		str = "<img id=\"component" + globalComponentCounter + "\" src=\"imgbtn.png\" title=\"imagebutton id=" + globalComponentCounter + "\"  height=\"80px\" width=\"80px\" class=\"drag\" />";
+		str = "<img id=\"component" + globalComponentCounter + "\" src=\"imgbtn.png\" title=\"imagebutton id=" + globalComponentCounter + "\"  height=\"80px\" width=\"80px\" class=\"drag component\" />";
+		break;
+		case 5:
+		str = "<div>horizontal layout</div>";
+		break;
+		case 6:
+		str = "<div>vertical layout</div>";
 		break;
 	}
+	
 	componentArray.push(createComponentObject(cClass, globalComponentCounter));
 	globalComponentCounter++;
 	return str;
 }
 
 var addToManager = function(cClass){
+	var str;
 	switch(cClass){
 		case 1:
 		str="<p>component" + (globalComponentCounter-1) + "(textview)</p>";
@@ -82,63 +127,21 @@ var addToManager = function(cClass){
 	return str;
 }
 
-//========= drag ====================
-
-var dragobject={
-	x:0, y:0,
-	
-	z: 0, tx: 0, ty: 0, offsetx : null, offsety : null, targetobj : null, dragapproved : 0,
-	
-	initialize:function(){
-		document.onmousedown=this.drag
-		document.onmouseup=function(){
-			this.dragapproved = 0
-		}
-
-	},
-	
-	drag:function(e){
-		var evtobj=window.event? window.event : e
-		this.targetobj=window.event? event.srcElement : e.target
-		if (this.targetobj.className=="drag"){
-			this.dragapproved=1
-			if (isNaN(parseInt(this.targetobj.style.left))){
-				this.targetobj.style.left=document.getElementById("widget_container").offsetLeft + "px";
-			}
-			if (isNaN(parseInt(this.targetobj.style.top))){
-				this.targetobj.style.top=document.getElementById("widget_container").offsetTop + "px";
-			}
-			this.offsetx=parseInt(this.targetobj.style.left)
-			this.offsety=parseInt(this.targetobj.style.top)
-			this.tx=evtobj.clientX
-			this.ty=evtobj.clientY
-			if (evtobj.preventDefault)
-				evtobj.preventDefault()
-			document.onmousemove=dragobject.moveit
-		}
-	},
-	
-	moveit:function(e){
-		var evtobj=window.event? window.event : e
-		if (this.dragapproved==1){
-			this.targetobj.style.left=this.offsetx+evtobj.clientX-this.tx+"px"
-			this.targetobj.style.top=this.offsety+evtobj.clientY-this.ty+"px"
-			this.x = this.offsetx+evtobj.clientX-this.tx;
-			this.y = this.offsety+evtobj.clientY-this.ty;
-			return false
-		}
-	},
-}
-dragobject.initialize()
-
 //============== component ==================
+/*属性：
+typename：组建类型名称，如textview，button，horizontalLayout等等
+id：组件的全局唯一ID
+parentID：组件的父节点ID
+
+*/
 componentArray = new Array();//存放所有的部件
 function createComponentObject(_typeName, _id){
 	component = new Object;
 	component.typeName=_typeName;
-	component.id=_id;	
+	component.id=_id;
 	return component;
 }
+
 
 
 
