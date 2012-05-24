776daa056464c0052aef7c76fb6f2aa6053332d0
Zongzi Wang <wzy.dtc@gmail.com>
2012-05-24 16:40:50 +0000

    Add files for #22
    git-svn-id: http://i.pku.edu.cn/svn/aaww@43 0219ba12-6bc3-4faa-bf3d-573e8937644a

--- web_design/script1.js
+++ web_design/script1.js
@@ -452,15 +452,44 @@ function getTag(_typeName) {
 }
 
 
+function ajaxFileUpload() {
+	$("#loading")
+	.ajaxStart(function(){
+		$(this).show();
+	})
+	.ajaxComplete(function(){
+		$(this).hide();
+	});
 
+	$.ajaxFileUpload
+	(
+		{
+			url:'doajaxfileupload.php',
+			secureuri:false,
+			fileElementId:'fileToUpload',
+			dataType: 'json',
+			data:{name:'logan', id:'id'},
+			success: function (data, status)
+			{
+				if(typeof(data.error) != 'undefined')
+				{
+					if(data.error != '')
+					{
+						alert(data.error);
+					}else
+					{
+						alert(data.msg);
+					}
+				}
+			},
+			error: function (data, status, e)
+			{
+				alert(e);
+			}
+		}
+	)
+	
+	return false;
 
-
-
-
-
-
-
-
-
-
+}
 
