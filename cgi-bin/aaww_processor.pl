71b1189a0abdc87af5ca127d0fb43318d2377bc8
Jiaye Xie <xiejiaye523@gmail.com>
2012-05-24 17:32:37 +0000

    Finished retrieving meta info from test_input.json (which will be replaced by data posted through web browser) and creating the Android project.
    git-svn-id: http://i.pku.edu.cn/svn/aaww@45 0219ba12-6bc3-4faa-bf3d-573e8937644a

--- cgi-bin/aaww_processor.pl
+++ cgi-bin/aaww_processor.pl
@@ -4,7 +4,10 @@ $LOG_DIR = "aaww_logs";
 if (!(-d $LOG_DIR)) {
 	mkdir $LOG_DIR;
 }
-$LOG_FILE = "$LOG_DIR/".localtime(time);
+use Date::Format;
+use Time::ParseDate;
+$time=time2str("%Y%m%d",parsedate(today));
+$LOG_FILE = "$LOG_DIR/$time.log";
 
 print "Content-type: text/html\n\n";
 
@@ -12,15 +15,42 @@ print "<html><head><title>Hello, World!</title></head>";
 print "<body>";
 print "<h2>Hello World!</h2>";
 
-$PROJ_NAME = "TestProject";
-$PROJ_TARGET = "1";
+#######################################################
+# Handling input from POST.
+#######################################################
+# read submitted form (by POST method) from stdin
+#read(STDIN, $buffer, $ENV{'CONTENT_LENGTH'});
+# split information into name/value pairs
+#@pairs = split(/&/, $buffer);
+#foreach $pair (@pairs) {
+#	($name, $value) = split(/=/, $pair);
+#	$value =~ tr/+/ /;
+#	$value =~ s/%(..)/pack("C", hex($1))/eg;
+#	$FORM{$name} = $value;
+#	print("$name: $value");
+#}
+#######################################################
+
+# for now when the web page hasn't been completed, we'll just get the input from a test file
+open(TEST_INPUT, "test_input.json") || die("cannot open input file");
+@input = <TEST_INPUT>;
+$input_string = join("", @input);
+
+# parse into JSON object
+use JSON;
+$json_obj = decode_json($input_string);
+
+$PROJ_NAME = $json_obj->{name};
+$PROJ_NAME =~ s/(\w+)/ucfirst(lc($1))/ge;
+$PROJ_TARGET = "2";
 $PROJ_PATH = "$PROJ_NAME";
-$PROJ_PKG = "aaww.test";
+$PROJ_PKG = "aaww.".$json_obj->{author};
+$PROJ_PKG =~ s/ //g;
 $PROJ_ACT = "MainActivity";
 
-$cmd = "../android-sdk-linux/tools/android create project --name $PROJ_NAME --target $PROJ_TARGET --path $PROJ_PATH --package $PROJ_PKG --activity $PROJ_ACT >> $LOG_FILE";
-system($cmd);
-
+$cmd = "date >> $LOG_FILE && ../android-sdk-linux/tools/android create project --name $PROJ_NAME --target $PROJ_TARGET --path $PROJ_PATH --package $PROJ_PKG --activity $PROJ_ACT >> $LOG_FILE";
+`$cmd`;
+#print "$cmd\n";
 
 
 print "</body></html>";
