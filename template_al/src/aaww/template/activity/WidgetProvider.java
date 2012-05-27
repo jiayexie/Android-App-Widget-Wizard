0fe7a1738c6f9bb825b5d037e9bd9a03da3721e0
Jiaye Xie <xiejiaye523@gmail.com>
2012-05-27 08:09:08 +0000

    Implemented forcing update the App Widget every 5 minutes.
    git-svn-id: http://i.pku.edu.cn/svn/aaww@60 0219ba12-6bc3-4faa-bf3d-573e8937644a

--- template_al/src/aaww/template/activity/WidgetProvider.java
+++ template_al/src/aaww/template/activity/WidgetProvider.java
@@ -1,5 +1,6 @@
 package aaww.template.activity;	// change package name to the user-specified one
 
+import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
@@ -8,10 +9,37 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
+import android.os.SystemClock;
 import android.widget.RemoteViews;
 
 public class WidgetProvider extends AppWidgetProvider {
+	
+	final static String ACTION_UPDATE = "aaww.template.activity.APPWIDGET_UPDATE";
+	final static long UPDATE_TIME = 5000 * 60; // 5 minutes
 
+	@Override
+	public void onEnabled(Context context) {
+		// Register an alarm to update reminder every UPDATE_TIME.
+		final Intent intent = new Intent(ACTION_UPDATE);
+		final PendingIntent pending = PendingIntent.getBroadcast(context, 0,
+				intent, 0);
+		final AlarmManager alarm = (AlarmManager) context
+				.getSystemService(Context.ALARM_SERVICE);
+		alarm.setRepeating(AlarmManager.ELAPSED_REALTIME,
+				SystemClock.elapsedRealtime(), UPDATE_TIME, pending);
+	}
+
+	@Override
+	public void onDisabled(Context context) {
+		// Cancel registered alarm.
+		final Intent intent = new Intent(ACTION_UPDATE);
+		final PendingIntent pending = PendingIntent.getBroadcast(context, 0,
+				intent, 0);
+		final AlarmManager alarm = (AlarmManager) context
+				.getSystemService(Context.ALARM_SERVICE);
+		alarm.cancel(pending);
+	}
+	
 	@Override
 	public void onDeleted(Context context, int[] appWidgetIds) {
 		super.onDeleted(context, appWidgetIds);
@@ -23,7 +51,8 @@ public class WidgetProvider extends AppWidgetProvider {
 	
 	@Override
 	public void onReceive(Context context, Intent intent) {
-		if (Intent.ACTION_BOOT_COMPLETED == intent.getAction()) {
+		String action = intent.getAction();
+		if (action == ACTION_UPDATE || action == Intent.ACTION_BOOT_COMPLETED) {
 			AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
 			ComponentName cn = new ComponentName(context, WidgetProvider.class);
 			int ids[] = appWidgetManager.getAppWidgetIds(cn);
